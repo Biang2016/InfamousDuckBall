c1909cf344fd2b01738bdea850451297b7ac7389
biang-mac <Biang@Biang-PC.(none)>
2020-02-17 00:32:25 -0500

    plugins

---
+++ Assets/Scripts/RotateArm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class RotateArm : MonoBehaviour
+{
+    public Vector3 RotateDir;
+    public string RotateKeyStr;
+    private KeyCode RotateKey;
+
+    void Awake()
+    {
+        RotateKey = (KeyCode) Enum.Parse(typeof(KeyCode), RotateKeyStr, true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(RotateKey))
+        {
+            transform.Rotate(RotateDir);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider c)
+    {
+        RotateArm RotateArm = c.gameObject.GetComponentInParent<RotateArm>();
+        if (RotateArm)
+        {
+            transform.localScale *= 2;
+        } 
+    }
+}
