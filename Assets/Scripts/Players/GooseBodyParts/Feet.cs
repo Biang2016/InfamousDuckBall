f95ad46af1de9bd0e27b53b18de43d7857596956
biang <12@212.com>
2020-03-31 20:08:18 +0800

    feet animation

--- Assets/Scripts/Players/GooseBodyParts/Feet.cs
+++ Assets/Scripts/Players/GooseBodyParts/Feet.cs
@@ -23,12 +23,69 @@ public class Feet : GooseBodyPart
     {
     }
 
+    [SerializeField] private Animator FeetAnimator;
+    [SerializeField] private float feetMoveThreshold = 1f;
+
+    public enum MoveStates
+    {
+        Static,
+        Forward,
+        Backward,
+    }
+
+    private MoveStates _moveState;
+
+    private MoveStates MoveState
+    {
+        get { return _moveState; }
+        set
+        {
+            if (_moveState != value)
+            {
+                switch (value)
+                {
+                    case MoveStates.Static:
+                    {
+                        FeetAnimator.SetTrigger("Static");
+                        break;
+                    }
+                    case MoveStates.Forward:
+                    {
+                        FeetAnimator.SetTrigger("Forward");
+                        break;
+                    }
+                    case MoveStates.Backward:
+                    {
+                        FeetAnimator.SetTrigger("Backward");
+                        break;
+                    }
+                }
+
+                _moveState = value;
+            }
+        }
+    }
+
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
         if (ParentPlayerControl.Player.PlayerControl.PlayerRigidbody.velocity.magnitude < BrakeVelocityThreshold)
         {
             ParentPlayerControl.Player.PlayerControl.PlayerRigidbody.velocity *= 0.9f;
+        }
+
+        float forward = Vector3.Dot(ParentPlayerControl.Player.PlayerControl.PlayerRigidbody.velocity, Goose.Body.BodyRotate.transform.forward);
+        if (forward > feetMoveThreshold)
+        {
+            MoveState = MoveStates.Forward;
+        }
+        else if (forward < -feetMoveThreshold)
+        {
+            MoveState = MoveStates.Backward;
+        }
+        else
+        {
+            MoveState = MoveStates.Static;
         }
     }
 }
