8e464ec64c66ea50fca22e21e602c4128b9d3e8f
RFZBAX <36533833+RFZBAX@users.noreply.github.com>
2020-04-23 00:30:53 -0400

    sound new 2

--- Assets/Scripts/Audio/AudioAPI.cs
+++ Assets/Scripts/Audio/AudioAPI.cs
@@ -5,7 +5,7 @@
         //玩家走路的音效调用
         //AudioDuck.Instance.StartPlayerMovementSound(playernumber, playerTransform, playerRigidbody);
         //鸭叫音效调用
-        //AudioDuck.Instance.StartPlayerQuackSound(playernumber,teamnumber, playerTransform, playerRB);
+        //AudioDuck.Instance.StartPlayerQuackSound(playernumber,teamnumber,action, playerTransform, playerRB);
 
         //调用音效
         //AudioDuck.Instance.PlaySound(AudioDuck.Instance.sound, gameObject);
@@ -15,7 +15,7 @@
         //停止刮风
         //AudioDuck.Instance.StopWind();
 
-        //蓄力——DuckCharge, 生成救生圈——DuckGenerateBuoy;
+        //蓄力——DuckCharge, 生成救生圈——DuckGenerateBuoy，鸭子碰到救生圈——DuckTouchBuoy;
         //河豚被捅——FishBreath, 河豚上岸摆动——FishFlapping;
         //放救生圈得分——BuoyInPlace, 救生圈爆炸——BuoyPop, 大海——Sea;
         
